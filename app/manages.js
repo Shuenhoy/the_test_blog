511b6e1f0e94d9d39b7ca920eb6d9a7bf65a2913
Shuenhoy <shuenhoy@gmail.com>
2013-04-28 20:38:59 +0800

    add all

---
+++ app/manages.js
@@ -0,0 +1,35 @@
+this.github = {
+	needUpdateTest: function(config,database) {
+
+		var info = JSON.parse(getSync('https://api.github.com/repos/' + config.manage.user + '/' + config.manage.repo + '/branches'));
+
+		if(database.version !== info[0].commit.sha) {
+			return info[0].commit.sha;
+		} else {
+			return false;
+		}
+	},
+	getlist: function(config) {
+		var list=JSON.parse(getSync('https://api.github.com/repos/'+config.manage.user+'/'+config.manage.repo+'/contents/post'));
+		
+		var tlist=[];
+		for (var i = list.length - 1; i >= 0; i--) {
+			
+			tlist[i]=list[i].name.replace(/^([0-9]*-[0-9]*-[0-9]*-)/,'').replace(/\.md$/,'');
+		};
+		console.log(tlist)
+		return tlist
+	}
+}
+this.manul = {
+	needUpdateTest: function(config, database) {
+		if (database.version !== config.manage.version || config.manage.always) {
+			return config.version || config.manage.always;
+		} else{
+			return false;
+		};
+	},
+	getlist: function() {
+		return jsyaml.load(getSync("list.yml"));
+	}
+}
